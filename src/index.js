8e6ae82b9eb09b56dfb17144dfda87bccbe1b4bb
rohan <rhnproctor@gmail.com>
2019-07-29 17:59:43 +1000

    init commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import * as serviceWorker from './serviceWorker';
+// import * as Tone from 'tone';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//create a synth and connect it to the master output (your speakers)
+// var synth = new Tone.Synth().toMaster()
+
+//play a middle 'C' for the duration of an 8th note
+// synth.triggerAttackRelease('C4', '8n')
+
+const store = configureStore();
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('root'));
+
+if (history.location.pathname === '/') {
+  history.push('/base');
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
